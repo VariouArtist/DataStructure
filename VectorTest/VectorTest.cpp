1c707281f5c845e8c24f21a0ad45896288d6a4e2
ytt <ytt@YTT1>
2024-01-29 21:16:00 +0800

    添加项目文件。

---
+++ VectorTest/VectorTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace VectorTest
+{
+	TEST_CLASS(VectorTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
